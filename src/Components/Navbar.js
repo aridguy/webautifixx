fd50fa6d97b8beeff50122a3431bfb4f4b994aaf
aridguy <50306833+aridguy@users.noreply.github.com>
2023-10-06 16:20:43 +0100

    modified

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,30 +1,40 @@
 const Navbar = () => {
   return (
     <div>
-      <nav className="container-fluid expand navars">
+      <nav className="container-fluid navars">
         <div className="container pt-4">
-          <div className="row">
+          <div className="d-flex gap-2">
             <div className="col-md-2 text-center">
-              <a className="black" href="https://github.com/walletconnect">Github</a>
+              <a className="black" href="https://github.com/walletconnect">
+                Github
+              </a>
             </div>
-            <div className="col-md-2 text-center">
-              <a className="black" href="https://github.com/walletconnect">Docs</a>
+            <div className="col-md-2 col-2 text-center">
+              <a className="black" href="https://github.com/walletconnect">
+                Docs
+              </a>
             </div>
-            <div className="col-md-4 text-center">
-              <img className="cursor" width='120' src='https://web3autofix.pages.dev/img/logo.37f2bc6e.svg' alt="site-logo" />
+            <div className="col-md-4 col-4 text-center">
+              <img
+                className="cursor"
+                width="120"
+                src="https://web3autofix.pages.dev/img/logo.37f2bc6e.svg"
+                alt="site-logo"
+              />
             </div>
-            <div className="col-md-2 text-center">
-              <a className="black" href="localhost:3000">Wallet</a>
+            <div className="col-md-2 col-2 text-center">
+              <a className="black" href="localhost:3000">
+                Wallet
+              </a>
             </div>
-            <div  className="col-md-2 text-center">
-              <a className="black" href="localhost:3000">Apps</a>
+            <div className="col-md-2 col-2 text-center">
+              <a className="black" href="localhost:3000">
+                Apps
+              </a>
             </div>
           </div>
         </div>
       </nav>
-
-      
-
     </div>
   );
 };
